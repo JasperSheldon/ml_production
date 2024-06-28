62218a5034b209a8afc6cf257926d90a5f99564f
JasperSheldon <sheldonjasper2009@gmail.com>
2024-06-28 21:06:39 +0530

    Adding schemas to fast api app

---
+++ app/schemas/predict.py
@@ -0,0 +1,37 @@
+from typing import Any, List,Optional
+
+from pydantic import BaseModel
+from classification_model.processing.validation import TitanicDataInputSchema
+
+
+class PredictionResults(BaseModel):
+    errors:Optional[Any]
+    version:str
+    predictions:Optional[List[int]]
+
+
+class MultipleTitanicInputs(BaseModel):
+    inputs:List[TitanicDataInputSchema]
+
+    class Config:
+        schema_extra={
+            "example":{
+                "inputs":[
+                    {
+                        "pclass": 1,
+                        "name": "Allison, Miss. Helen Loraine",
+                        "sex": "female",
+                        "age": 2,
+                        "sibsp": 1,
+                        "parch": 2,
+                        "ticket": 113781,
+                        "fare": 151.55,
+                        "cabin": "C22",
+                        "embarked": "S",
+                        "boat": None,
+                        "body": None
+                    }
+                ]
+            }
+        }
+
